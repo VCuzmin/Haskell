5e57b67ed4d56c348feea5b62c007185c9f416ae
VCuzmin <vcuzmin@totalsoft.ro>
2020-07-26 10:51:34 +0300

    Lesson 8. Writing recursive functions

--- lambda_functions.hs
+++ lambda_functions.hs
@@ -1,6 +1,6 @@
--- One of the most foundational concepts in functional programming is a function without a name, called a lambda function (hence lambda calculus). 
--- Lambda functions are often referred to using the lowercase Greek letter λ. 
--- Another common name for a lambda function is an anonymous function. 
+-- One of the most foundational concepts in functional programming is a function without a name, called a lambda function (hence lambda calculus).
+-- Lambda functions are often referred to using the lowercase Greek letter λ.
+-- Another common name for a lambda function is an anonymous function.
 
 -- GHCi> (\x -> x) 4
 -- 4
@@ -8,61 +8,66 @@
 -- hi
 -- GHCi> (\x -> x) [1,2,3]
 -- [1,2,3]
--- Notice that each time you use your lambda expression, you have to redefine it. 
--- This makes sense, because you have no name to call it by! 
+-- Notice that each time you use your lambda expression, you have to redefine it.
+-- This makes sense, because you have no name to call it by!
 -- Lambda functions are useful but are designed to exist for only a short while.
 -- In general, if a named function will do the job, it’s better to use one.
 
-
 --with Where clause
-sumSquareOrSquareSum x y = if sumSquare > squareSum
-                           then sumSquare
-                           else squareSum
-  where sumSquare = x^2 + y^2
-        squareSum = (x+y)^2
+sumSquareOrSquareSum :: (Ord p, Num p) => p -> p -> p
+sumSquareOrSquareSum x y =
+  if sumSquare > squareSum
+    then sumSquare
+    else squareSum
+  where
+    sumSquare = x ^ 2 + y ^ 2
+    squareSum = (x + y) ^ 2
 
 --without Where clause -- hideous, horrible, ugly code! - a lot of duplication
-sumSquareOrSquareSum_v2 x y = if (x^2 + y^2) > ((x + y)^2)
-                            then (x^2 + y^2)
-                            else (x+y)^2
+sumSquareOrSquareSum_v2 :: (Num a, Ord a) => a -> a -> a
+sumSquareOrSquareSum_v2 x y =
+  if (x ^ 2 + y ^ 2) > ((x + y) ^ 2)
+    then (x ^ 2 + y ^ 2)
+    else (x + y) ^ 2
 
 --better
-body sumSquare squareSum = if sumSquare > squareSum
-                        then sumSquare
-                        else squareSum
+body :: Ord p => p -> p -> p
+body sumSquare squareSum =
+  if sumSquare > squareSum
+    then sumSquare
+    else squareSum
 
-sumSquareOrSquareSum_v3 x y = body (x^2 + y^2) ((x + y)^2)
-
+sumSquareOrSquareSum_v3 :: (Num a, Ord a) => a -> a -> a
+sumSquareOrSquareSum_v3 x y = body (x ^ 2 + y ^ 2) ((x + y) ^ 2)
 
 --much better - with lambda function
-body_v2 = (\sumSquare squareSum -> 
-       if sumSquare > squareSum
-           then sumSquare
-           else squareSum)
+body_v2 :: Integer -> Integer -> Integer
+body_v2 =
+  ( \sumSquare squareSum ->
+      if sumSquare > squareSum
+        then sumSquare
+        else squareSum
+  )
 
-sumSquareOrSquareSum_v4 x y = body_v2 (x^2 + y^2) ((x + y)^2)
+sumSquareOrSquareSum_v4 :: Integer -> Integer -> Integer
+sumSquareOrSquareSum_v4 x y = body_v2 (x ^ 2 + y ^ 2) ((x + y) ^ 2)
 
-
-
-
--- Haskell has an alternative to where clauses called let expressions. 
+-- Haskell has an alternative to where clauses called let expressions.
 -- A let expression allows you to combine the readability of a where clause with the power of your lambda function.
 
-sumSquareOrSquareSum_v5 x y = let sumSquare = (x^2 + y^2)
-                                  squareSum = (x + y)^2
-                              in
-                                if sumSquare > squareSum
-                                    then sumSquare
-                                    else squareSum
+sumSquareOrSquareSum_v5 :: (Ord p, Num p) => p -> p -> p
+sumSquareOrSquareSum_v5 x y =
+  let sumSquare = (x ^ 2 + y ^ 2)
+      squareSum = (x + y) ^ 2
+   in if sumSquare > squareSum
+        then sumSquare
+        else squareSum
 
 -- Whether you choose to use let or where is a matter of style the vast majority of the time in Haskell.
 
-
-
--- Whenever you create a new function, named or not, you create a new scope, which is the context in which a variable is defined. 
--- When a variable is used, the program looks at the nearest scope; if the definition of the variable isn’t there, it goes to the next one up. 
--- This particular type of variable lookup is called lexical scope. 
+-- Whenever you create a new function, named or not, you create a new scope, which is the context in which a variable is defined.
+-- When a variable is used, the program looks at the nearest scope; if the definition of the variable isn’t there, it goes to the next one up.
+-- This particular type of variable lookup is called lexical scope.
 -- Both Haskell and JavaScript use lexical scoping, which is why IIFE and your lambda function variables behave in a similar fashion
-
 
 -- Being able to use unnamed functions to create scope on the fly is an essential tool for doing much more powerful things with lambda functions
--- recursion.hs
+++ recursion.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 -- GREATEST COMMON DIVISOR
 
 -- a = 20, b = 16
@@ -6,15 +8,72 @@
 -- a/b = 4 remainder 0
 -- GCD = b = 4
 
-myGCD a b = if remainder == 0
-            then b
-            else myGCD b remainder
-  where remainder = a `mod` b
+myGCD :: Integral t => t -> t -> t
+myGCD a b =
+  if remainder == 0
+    then b
+    else myGCD b remainder
+  where
+    remainder = a `mod` b
+myGCD a 0 = a -- version with pattern matching
+myGCD a b = myGCD b (a `mod` b)
 
-
-myGCD a 0 = a   -- version with pattern matching
-myGCD a b = myGCD b (a 'mod' b)
-
-
-myHead (x:xs) = x
+myHead :: [p] -> p
+myHead (x : xs) = x
 myHead [] = error "No head for empty list"
+
+-- RECURSION ON LISTS
+
+myLength :: Num a1 => [a2] -> a1
+myLength [] = 0
+myLength xs = 1 + myLength (tail xs)
+--or
+
+myLength [] = 0
+myLength (x : xs) = 1 + myLength (xs)
+
+--  Implementing take
+
+-- The take function is interesting for two reasons: take uses two arguments, n and a list, and it turns out take has two goal states!
+-- As is almost always the case, take terminates on the empty list [].
+-- As mentioned earlier, unlike tail and head, take has no problem with the empty list, and will return as many items as it can.
+-- The other condition when take can be finished occurs when n = 0. In either case, you end up doing the same thing.
+-- Taking n elements from an empty list is [], and taking 0 elements of any list is [].
+-- So you end up with this:
+
+myTake :: Num [a1] => [a1] -> [a2] -> [a2]
+myTake _ [] = []
+myTake [] _ = []
+-- Let’s think about this with take 3 [1,2,3,4,5]:
+-- You want the first element, 1, and then cons that along with take 2 [2,3,4,5].
+-- Then you want the next element, 2, and cons it with take 1 [3,4,5].
+-- Then you want 3 and cons it with take 0 [4,5].
+-- At 0 you’ve reached a goal, so return [].
+-- This leads to 1:2:3:[], which is [1,2,3].
+
+myTake n (x : xs) = x : rest
+  where
+    rest = myTake (n -1) xs
+
+--  Implementing reverse
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x : []) = [x]
+myReverse (x : xs) = myReverse (xs) ++ [x]
+
+
+fib :: (Eq a, Num a, Num p) => a -> p
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+
+fastFib _ _ 0 = 0
+fastFib _ _ 1 = 1
+fastFib _ _ 2 = 1
+fastFib x y 3 = x + y
+fastFib x y c = fastFib (x + y) x (c - 1)
+
+fib_v2 :: (Eq t1, Num t1, Num t2) => t1 -> t2
+fib_v2 n = fastFib 1 1 n
