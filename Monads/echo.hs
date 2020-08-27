7a552025c97402d5fa1c24b0d210ebdb568145e5
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2020-08-27 14:08:20 +0300

    sugarring

--- Monads/echo.hs
+++ Monads/echo.hs
@@ -3,3 +3,17 @@ echo = getLine >>= putStrLn
 
 main :: IO ()
 main = echo
+
+--or
+
+-- echo :: IO ()
+-- echo = getLine >>= putStrLn
+
+-- main :: IO ()
+ -- main = do 
+--    text <- getLine
+--    putStrLn text
+
+
+
+-- do x <- f; g x is sugar for f >>= (\x -> g x)
